39c747d5d62033e99856be250eaec696478ea4fc
coder <103135051+uzxakker@users.noreply.github.com>
2022-12-15 22:51:11 +0500

    hero pull  code

--- src/layouts/Home/index.jsx
+++ src/layouts/Home/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import HomeComponent from "../../components/HomeComponent";
 function Home() {
-  const {t , i18n}= useTranslation();
+  const { t, i18n } = useTranslation();
   return (
-    <div>
-      {t("Header.0")}
-    </div>
-  )
+    <>
+      {/* {t("Header.0")} */}
+      <HomeComponent />
+    </>
+  );
 }
 
-export default Home
+export default Home;
