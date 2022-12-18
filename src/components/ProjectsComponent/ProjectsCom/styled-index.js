5572c76836ac5e31b94377cb96b1d04054dc76da
coder <103135051+uzxakker@users.noreply.github.com>
2022-12-18 17:10:03 +0500

    projects unit push

---
+++ src/components/ProjectsComponent/ProjectsCom/styled-index.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  padding: 60px 0;
+`;
+
+export const BeforeContainer = styled.div`
+  max-width: 98%;
+  width: 100%;
+  margin: 0 auto;
+
+  .slider {
+    height: 557px;
+    width: 100px;
+    width: 2px;
+    z-index: 1;
+    background: #fff;
+  }
+   .row{
+    margin: 0 !important;
+   }
+   .big-col{
+    padding: 0 !important;
+    margin: 20px 0 !important;
+   }
+  .small-row{
+    margin: 0 !important;
+  }
+  .col {
+    padding: 0 !important;
+  }
+
+  .white-col {
+    background: #fff;
+    padding: 0 !important;
+    height: 557px;
+
+    h3 {
+      color: #000;
+      font-weight: 300;
+      font-family: "Ruda";
+      font-size: 25px;
+      line-height: 38px;
+      letter-spacing: 0.1em;
+      text-transform: uppercase;
+    }
+    p {
+      color: #000;
+      font-family: "Ruda";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 27px;
+    }
+  }
+  .col-content {
+    width: 100%;
+    height: 100%;
+    padding: 10px;
+  }
+`;
