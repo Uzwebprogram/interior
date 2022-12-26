474eb46b71f2f704e3b7d8899308cfc91fc9278a
Obidjonov Abdulloh <suprablack12@gmail.com>
2022-12-26 22:19:12 +0500

    redux push

--- src/components/HomeComponent/Trusted/index.jsx
+++ src/components/HomeComponent/Trusted/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Section } from "./styled-index";
 import { Row, Col } from "react-grid-system";
 import { useTranslation } from "react-i18next";
@@ -15,51 +15,61 @@ import logoIcon9 from "../../../assets/image/Home/icon9.png";
 import logoIcon10 from "../../../assets/image/Home/icon10.png";
 import logoIcon11 from "../../../assets/image/Home/icon11.png";
 import logoIcon12 from "../../../assets/image/Home/icon12.png";
+import { useDispatch, useSelector } from "react-redux";
+import { Gettrusted } from "../../../redux/trusted";
 
 const Trusted = () => {
   const { t, i18n } = useTranslation();
+  const dispatch = useDispatch();
+  const getTrusted = useSelector((state) => state.trusted.gettrusted?.Data);
+  console.log(getTrusted);
+  useEffect(() => {
+    dispatch(Gettrusted());
+  }, []);
   return (
     <>
       <Section>
         <div className="trustContainer">
           <h2>{t("Trusted.0")}</h2>
           <Row className="row">
-            <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon1} alt="logo" />
+            {getTrusted.map((elem, index) => (
+              <Col key={index} className="col" lg={2} md={4} sm={12} sx={6}>
+                <img width={207} height={170} src={elem.partners_img} alt="logo" />
+              </Col>
+            ))}
+            {/* <Col className="col" lg={2} md={4} sm={12} sx={6}>
+              <img src={logoIcon6} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon6} alt="logo" />
+              <img src={logoIcon11} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon11} alt="logo" />
+              <img src={logoIcon10} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon10} alt="logo" />
+              <img src={logoIcon2} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon2} alt="logo" />
+              <img src={logoIcon3} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon3} alt="logo" />
+              <img src={logoIcon12} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon12} alt="logo" />
+              <img src={logoIcon9} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon9} alt="logo" />
+              <img src={logoIcon9} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon9} alt="logo" />
+              <img src={logoIcon7} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon7} alt="logo" />
+              <img src={logoIcon4} alt="logo" />
             </Col>
             <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon4} alt="logo" />
-            </Col>
-            <Col className="col" lg={2} md={4} sm={12} sx={6}>
-               <img src={logoIcon5} alt="logo" />
-            </Col>
+              <img src={logoIcon5} alt="logo" />
+            </Col> */}
           </Row>
         </div>
       </Section>
